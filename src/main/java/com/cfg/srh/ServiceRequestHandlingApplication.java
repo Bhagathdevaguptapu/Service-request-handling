fbd8cce9de8cc34c3f8c63a3a563538470057101
laharimuppalla <110014328+laharimuppalla@users.noreply.github.com>
2025-07-18 16:31:09 +0530

    Add files via upload

---
+++ src/main/java/com/cfg/srh/ServiceRequestHandlingApplication.java
@@ -0,0 +1,13 @@
+package com.cfg.srh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRequestHandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRequestHandlingApplication.class, args);
+	}
+
+}
